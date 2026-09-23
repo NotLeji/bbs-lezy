d986051fe320dd2317a8715a823cc273902eac46
Leji2807 <ridhobangun2807@gmail.com>
2026-09-23 16:04:00 +0700

    feat: switch LOD popup from slider to trackpads for custom typing and dragging
    - Replace UISliderTrackpad with UITrackpad in UILodContextMenu
    - Users can drag to adjust continuously or click to type custom numbers directly
    - Stack labels vertically above trackpads matching BBS context menu styling

--- src/main/java/bbslezy/ui/UILodContextMenu.java
+++ src/main/java/bbslezy/ui/UILodContextMenu.java
@@ -6,7 +6,7 @@ import mchorse.bbs_mod.ui.framework.UIContext;
 import mchorse.bbs_mod.ui.framework.elements.UIElement;
 import mchorse.bbs_mod.ui.framework.elements.buttons.UIToggle;
 import mchorse.bbs_mod.ui.framework.elements.context.UIContextMenu;
-import mchorse.bbs_mod.ui.framework.elements.input.UISliderTrackpad;
+import mchorse.bbs_mod.ui.framework.elements.input.UITrackpad;
 import mchorse.bbs_mod.ui.utils.UI;
 
 public class UILodContextMenu extends UIContextMenu
@@ -21,24 +21,28 @@ public class UILodContextMenu extends UIContextMenu
         });
         enabled.setValue(LodSettings.enabled.get());
 
-        UISliderTrackpad limit = new UISliderTrackpad((v) ->
+        UITrackpad limit = new UITrackpad((v) ->
         {
             LodSettings.renderLimit.set(v.intValue());
         });
-        limit.limit(LodSettings.renderLimit).setValue(LodSettings.renderLimit.get());
+        limit.limit(LodSettings.renderLimit).values(5D).increment(1D).setValue(LodSettings.renderLimit.get());
+        limit.tooltip(L10n.lang("bbslezy.config.general.render_limit-comment"));
 
-        UISliderTrackpad focus = new UISliderTrackpad((v) ->
+        UITrackpad focus = new UITrackpad((v) ->
         {
             LodSettings.focusDistance.set(v.doubleValue());
         });
-        focus.limit(LodSettings.focusDistance).increment(5D).values(1D, 0.5D, 5D).setValue(LodSettings.focusDistance.get());
+        focus.limit(LodSettings.focusDistance).values(1D).increment(1D).setValue(LodSettings.focusDistance.get());
+        focus.tooltip(L10n.lang("bbslezy.config.general.focus_distance-comment"));
 
-        this.column = UI.column(5, 8,
+        this.column = UI.column(4, 6,
             enabled,
-            UI.labelRow(L10n.lang("bbslezy.ui.lod.render_limit"), 80, limit),
-            UI.labelRow(L10n.lang("bbslezy.ui.lod.focus_distance"), 80, focus)
+            UI.label(L10n.lang("bbslezy.ui.lod.render_limit")),
+            limit,
+            UI.label(L10n.lang("bbslezy.ui.lod.focus_distance")),
+            focus
         );
-        this.column.relative(this).w(220);
+        this.column.relative(this).w(150);
 
         this.add(this.column);
         this.column.resize();
